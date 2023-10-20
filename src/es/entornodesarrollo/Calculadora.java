5a71f36b716bba05420d22670f42e0ea0fce0552
Francisco-Aidenblue2 <fadavic@iesmarquesdecomares.org>
2023-10-20 22:01:38 +0200

    Multiplicación

--- src/es/entornodesarrollo/Calculadora.java
+++ src/es/entornodesarrollo/Calculadora.java
@@ -5,6 +5,7 @@ public class Calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1. Suma");
+		System.out.println("3. Multiplicación");
 	}
 
 }
